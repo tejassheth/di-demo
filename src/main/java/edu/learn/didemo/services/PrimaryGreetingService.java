4356a1a607efd7a9e0bc6e3389d8b2100bde0d22
tejassheth <sheth.t.j.2012@gmail.com>
2019-10-02 08:18:13 -0400

    adding factory bean example

--- src/main/java/edu/learn/didemo/services/PrimaryGreetingService.java
+++ src/main/java/edu/learn/didemo/services/PrimaryGreetingService.java
@@ -4,13 +4,10 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Service
-@Primary
-@Profile({"en","default"})
-public class PrimaryGreetingservice implements GreetingService {
+public class PrimaryGreetingService implements GreetingService {
     private GreetingRepository greetingRepository;
 
-    public PrimaryGreetingservice(GreetingRepository greetingRepository) {
+    public PrimaryGreetingService(GreetingRepository greetingRepository) {
         this.greetingRepository = greetingRepository;
     }
 
