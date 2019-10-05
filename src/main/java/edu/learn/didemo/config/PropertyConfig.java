7b547375bfa044fe3cf4d9a3ab3ee92404bd7cb2
Tejas Sheth <sheth.t.j.2012@gmail.com>
2019-10-06 01:51:04 +0530

    add system variable USERNAME and found that it is override by properties configuration

--- src/main/java/edu/learn/didemo/config/PropertyConfig.java
+++ src/main/java/edu/learn/didemo/config/PropertyConfig.java
@@ -1,15 +1,19 @@
 package edu.learn.didemo.config;
 
 import edu.learn.didemo.examplebean.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+    @Autowired
+    Environment environment;
     @Value("${guru.username}")
     String username;
 
@@ -22,7 +26,7 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource fakeDataSource(){
         FakeDataSource fakeDataSource= new FakeDataSource();
-        fakeDataSource.setUsername(username);
+        fakeDataSource.setUsername(environment.getProperty("username"));
         fakeDataSource.setURL(URL);
         fakeDataSource.setPassword(password);
         return  fakeDataSource;
